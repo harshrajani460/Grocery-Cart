861c7f72934b936634c9322ebbb96a908e4bff93
harshrajani460 <harshrajani460@gmail.com>
2021-06-18 16:55:42 +0530

    Added features

--- src/index.js
+++ src/index.js
@@ -22,32 +22,46 @@ function validateItemName(name) {
 
 function addItem(event) {
   let placeItemName = document.getElementById("inp-item");
-
+  let placeQt = document.getElementById("inp-qt");
+  if (placeQt.value === "") document.getElementById("inp-qt").value = "0";
+  placeQt.value = Number(placeQt.value).toString();
+  let newName = placeItemName.value.trim();
+  document.getElementById("inp-item").value = newName;
   if (!validateItemName(placeItemName.value)) {
     return alert("Please Enter Name");
   }
 
   if (event.target.value === "add-item") {
-    addItemTrue(event);
+    addItemTrue(event); //When Add workflow is on
   } else {
     placeItemName.removeAttribute("readonly");
-    addItemFalse(event);
+    addItemFalse(event); //When Edit workflow is on
   }
 }
 function createFromLocalStorage(element) {
   let div = document.createElement("div");
   let innerdiv = document.createElement("div");
+  let innerdiv1 = document.createElement("div");
+  let innerdiv2 = document.createElement("div");
   let btn1 = document.createElement("button");
   let btn2 = document.createElement("button");
 
   btn1.innerText = "Edit";
   btn1.value = element.qt;
   btn2.innerText = "Delete";
-  innerdiv.innerText = element.name;
+
+  innerdiv1.innerText = element.name;
+  innerdiv2.innerText = "Qty. : " + element.qt;
   div.classList.add("item");
   innerdiv.classList.add("item-text");
+  innerdiv1.classList.add("item-text-1");
+  innerdiv2.classList.add("item-text-2");
+
   btn1.classList.add("item-edit");
   btn2.classList.add("item-delete");
+
+  innerdiv.appendChild(innerdiv1);
+  innerdiv.appendChild(innerdiv2);
   div.appendChild(innerdiv);
   div.appendChild(btn1);
   div.appendChild(btn2);
@@ -61,7 +75,7 @@ function deleteFromLocalStorage(name) {
   let str = localStorage.getItem("groceryList");
   let arr = JSON.parse(str);
   let newArr = arr.filter((item) => {
-    if (item.name == name) {
+    if (item.name === name) {
       return false;
     }
     return true;
@@ -72,7 +86,7 @@ function editFromLocalSorage(oldName, oldqt, newName, newqt) {
   let str = localStorage.getItem("groceryList");
   let arr = JSON.parse(str);
   let newArr = arr.map((item) => {
-    if (item.name == oldName) {
+    if (item.name === oldName) {
       return { ...item, name: newName, qt: newqt };
     }
     return item;
@@ -85,7 +99,7 @@ function isAlready(name) {
   if (str != null) {
     let arr = JSON.parse(str);
     arr.forEach((item) => {
-      if (item.name == name) available = true;
+      if (item.name === name) available = true;
     });
   }
   return available;
@@ -96,7 +110,7 @@ function updateQuantity(name, quantity) {
   let id = -1;
   let newqt = 0;
   let newArr = arr.map((item, index) => {
-    if (item.name == name) {
+    if (item.name === name) {
       let newQuantity = String(Number(item.qt) + Number(quantity));
       id = index;
       newqt = newQuantity;
@@ -107,6 +121,7 @@ function updateQuantity(name, quantity) {
   localStorage.setItem("groceryList", JSON.stringify(newArr));
   let listitem = document.querySelectorAll(".item");
   listitem[id].childNodes[1].value = newqt;
+  listitem[id].childNodes[0].childNodes[1].innerText = "Qty. : " + newqt;
   let olditemName = document.getElementById("inp-item");
   let oldquantity = document.getElementById("inp-qt");
   olditemName.value = "";
@@ -126,16 +141,29 @@ function addItemTrue() {
     }
     let div = document.createElement("div");
     let innerdiv = document.createElement("div");
+    let innerdiv1 = document.createElement("div");
+    let innerdiv2 = document.createElement("div");
+
     let btn1 = document.createElement("button");
     let btn2 = document.createElement("button");
+
     btn1.innerText = "Edit";
     btn1.value = quantity.value;
     btn2.innerText = "Delete";
-    innerdiv.innerText = itemName.value;
+
+    innerdiv1.innerText = itemName.value;
+    innerdiv2.innerText = "Qty. : " + quantity.value;
+
+    innerdiv1.classList.add("item-text-1");
+    innerdiv2.classList.add("item-text-2");
     div.classList.add("item");
     innerdiv.classList.add("item-text");
     btn1.classList.add("item-edit");
     btn2.classList.add("item-delete");
+
+    innerdiv.appendChild(innerdiv1);
+    innerdiv.appendChild(innerdiv2);
+
     div.appendChild(innerdiv);
     div.appendChild(btn1);
     div.appendChild(btn2);
@@ -168,7 +196,7 @@ function addItemFalse() {
   let placeItemName = document.getElementById("inp-item");
   let placeQuantity = document.getElementById("inp-qt");
   editFromLocalSorage(
-    currentEditedEle.childNodes[0].innerText,
+    currentEditedEle.childNodes[0].childNodes[0].innerText,
     currentEditedEle.childNodes[1].value,
     placeItemName.value,
     placeQuantity.value
@@ -176,7 +204,9 @@ function addItemFalse() {
   let name = document.getElementById("action-heading");
   name.innerText = "Add Grocery Item";
 
-  currentEditedEle.childNodes[0].innerText = placeItemName.value;
+  currentEditedEle.childNodes[0].childNodes[0].innerText = placeItemName.value;
+  currentEditedEle.childNodes[0].childNodes[1].innerText =
+    "Qty. : " + placeQuantity.value;
   placeItemName.value = "";
 
   currentEditedEle.childNodes[1].value = placeQuantity.value;
@@ -185,13 +215,14 @@ function addItemFalse() {
   let btn = document.getElementById("add-btn");
   btn.value = "add-item";
   btn.innerText = "Add Item";
+  currentEditedEle = "";
 }
 function editItem(event) {
   let name = document.getElementById("action-heading");
   let placeItemName = document.getElementById("inp-item");
   placeItemName.setAttribute("readonly", true);
   name.innerText = "Edit Grocery Item";
-  let itemName = event.target.parentNode.childNodes[0].innerText;
+  let itemName = event.target.parentNode.childNodes[0].childNodes[0].innerText;
   placeItemName.value = itemName;
 
   let quantity = event.target.value;
@@ -204,16 +235,20 @@ function editItem(event) {
   currentEditedEle = event.target.parentNode;
 }
 function deleteItem(event) {
-  totalItem--;
-  let parent = event.target.parentNode;
-  deleteFromLocalStorage(
-    parent.childNodes[0].innerText,
-    parent.childNodes[1].value
-  );
-  parent.remove();
-  if (totalItem === 0) {
-    let nothing = document.getElementById("nothing");
-    nothing.style.display = "block";
+  if (currentEditedEle !== event.target.parentNode) {
+    totalItem--;
+    let parent = event.target.parentNode;
+    deleteFromLocalStorage(
+      parent.childNodes[0].childNodes[0].innerText,
+      parent.childNodes[1].value
+    );
+    parent.remove();
+    if (totalItem === 0) {
+      let nothing = document.getElementById("nothing");
+      nothing.style.display = "block";
+    }
+  } else {
+    alert("You can not delete item that are from Edit section.");
   }
 }
 
